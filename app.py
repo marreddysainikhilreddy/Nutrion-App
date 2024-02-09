5793d1b2e9d8940311536edd778d421f1a633c31
Marreddy Sai Nikhil Reddy <sainikhilreddym2000@gmail.com>
2024-02-08 16:20:44 -0800

    Adding code for nutrition app

---
+++ app.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+# Loading all environment variables
+load_dotenv()  
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(input_prompt, image):
+    # for images
+    model = genai.GenerativeModel("gemini-pro-vision")
+    response = model.generate_content([input_prompt, image[0]])
+    return response.text
+
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if(uploaded_file is not None):
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+
+# Initializing the streamlit app frontend
+    
+st.set_page_config(page_title="Gemini Nutrition App")
+
+st.header("Gemini Nutrition App")
+uploaded_file = st.file_uploader("Choose an Image...", type=["jpg", "jpeg", "png"])
+image = ""
+if(uploaded_file is not None):
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+submit = st.button("Give me information about total calories")
+
+
+input_prompt = """
+You are an expert in nutritionist where you need to see the food items from the range 
+                and calculate the total calories, also provide the details of 
+                every food items with calories intake
+                in below format
+
+                1. Item 1 - no of calories
+                2. Item 2 - no of calories
+                ----
+                ----
+        Finally you can also mention whether the food is healthy or not and also mention the percentage 
+        split of the ratio of carbohydrates, fats, fibres, sugar and things required in our diet
+"""
+
+if(submit):
+    image_data = input_image_setup(uploaded_file)
+    response = get_gemini_response(input_prompt, image_data)
+    st.header("The Response is")
+    st.write(response)
